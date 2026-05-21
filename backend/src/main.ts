d9e31b5b8b444ada0f277691a4899c577579e98f
Shebakn <amgd77uu@gmail.com>
2026-05-21 19:38:01 +0300

    Solve CROS problem

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -101,8 +101,12 @@ async function bootstrap() {
   // START SERVER
   // =========================
   app.enableCors({
-    origin: "*",
-  });
+  origin: [
+    "http://localhost:5173",
+    "https://full-stack-e-cummerce-store.vercel.app/"
+  ],
+  credentials: true,
+});
 
   const port = process.env.PORT || 3000;
 
