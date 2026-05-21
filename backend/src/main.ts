01b4071c12181b2df50b0f57fd79a52f31012448
Shebakn <amgd77uu@gmail.com>
2026-05-21 20:05:43 +0300

    Solve CROS4 problem

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -103,7 +103,9 @@ async function bootstrap() {
   app.enableCors({
   origin: [
     "http://localhost:5173",
-    "https://full-stack-e-cummerce-store.vercel.app"
+    "https://full-stack-e-cummerce-store.vercel.app",
+    "https://full-stack-e-cummerce-store.vercel.app/login",
+    "https://full-stack-e-cummerce-store.vercel.app/register",
   ],
   credentials: true,
 });
